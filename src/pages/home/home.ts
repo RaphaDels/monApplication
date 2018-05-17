e7e47b968d099f27cf752f664288a90908e42876
raphadels <raphaeledelsemme@gmail.com>
2018-05-17 16:27:42 +0200

    avec page a propos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,12 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  //je crée la fonction que j'ai mise dans home.html
+  goToAbout(){
+    //je change la page d'accueil par la page à propos au clic sur le bouton
+    this.navCtrl.push(AboutPage);
   }
 
 }
